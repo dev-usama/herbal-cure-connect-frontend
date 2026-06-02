fcb77aada1c14dd280b7532087d4be66fc0258dc
Usama Ahmed <minhasusama66@gmail.com>
2026-06-02 18:59:49 +0500

    Add loader for verifying session

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -6,7 +6,12 @@ const ProtectedRoute = () => {
     const { isAuthenticated, loading } = useContext(AuthContext);
 
     if (loading) {
-        return <div>Verifying session...</div>;
+        return (
+            <div className="flex justify-center items-center h-screen">
+                <div
+                    className="w-16 h-16 border-4 border-green-600 border-t-transparent rounded-full animate-spin"></div>
+            </div>
+        );
     }
 
     return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
