b1cfc1c4b26ce0ad351d840e68ee2659b58c3439
Usama Ahmed <minhasusama66@gmail.com>
2026-07-05 14:10:11 +0500

    Add form validation to login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,23 +1,16 @@
-import { useState, useContext } from 'react';
+import { useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
+import { useForm } from 'react-hook-form';
 
 export default function Login() {
-    const [formData, setFormData] = useState({ email: "", password: "" });
+    const { register, handleSubmit, setError, formState: { errors } } = useForm();
     const navigate = useNavigate();
     const { login } = useContext(AuthContext);
 
-    const handleFormChange = function (e) {
-        let fieldName = e.target.name;
-        let newValue = e.target.value;
-        setFormData((data) => {
-            return { ...data, [fieldName]: newValue }
-        })
-    }
-
-    const authUser = async (e) => {
-        e.preventDefault();
+    const authUser = async (formData) => {
         try {
+            console.log(formData);
             const apiUrl = import.meta.env.VITE_BACKEND_URL;
             const response = await fetch(apiUrl + '/users/login', {
                 method: 'POST',
@@ -30,7 +23,9 @@ export default function Login() {
                 login(responseData.data);
                 navigate('/dashboard');
             } else {
-                alert('Login failed. Server responded with status: ' + response.status + ' - ' + response.statusText);
+                setError("password", {
+                    message: response.status === 401 ? response.text() : "Login failed. Please check your credentials."
+                })
             }
         } catch (error) {
             console.error('Login error:', error);
@@ -42,15 +37,24 @@ export default function Login() {
                 <div className="login-form-overlay">
                     <h1>Welcome to Herbal Cure!</h1>
                     <p>Enter your Credentials to access your account</p>
-                    <form className='loginForm' onSubmit={authUser}>
+                    <form className='loginForm' onSubmit={handleSubmit(authUser)}>
                         <label htmlFor='emailInput'>Email address</label><br />
-                        <input id="emailInput" type='email' name="email" value={formData.email} onChange={handleFormChange} placeholder='Enter your email' /><br />
+                        <input id="emailInput" type='email' {...register('email', {
+                            required: "Email is required"
+                        })} placeholder='Enter your email' /><br />
+                        {errors.email && <span role="alert">{errors.email?.message}</span>}
                         <div>
                             <label htmlFor='passwordInput'>Password</label>
                             <a href="#">Forgot Password</a>
                         </div>
-                        <input id='passwordInput' name="password" type="password" value={formData.password} onChange={handleFormChange} placeholder='Enter your password' /><br />
+                        <input id='passwordInput' name="password" type="password" {...register('password', {
+                            required: "Password is required", minLength: {
+                                value: 5, message: "Password must be at least 9 characters long"
+                            }
+                        })} placeholder='Enter your password' /><br />
+                        {errors.password && <span role="alert">{errors.password?.message}</span>}
                         <button type="submit">Login</button>
+                        { }
                     </form>
                     <div className='redirectToRegisterDiv'>Don&apos;t have an account? <Link to="/register">&nbsp;Sign Up</Link></div>
                 </div>
